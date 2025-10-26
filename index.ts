7989650b2bab32de8c52f3af2fa7039f8b2a4998
Nikita Levchuk <code@levch.uk>
2025-10-26 22:34:20 +0100

    refactor: return values of all functions are consistent
    * All arguments are mandatory but can be undefined

--- index.ts
+++ index.ts
@@ -1,20 +1,24 @@
 import type { Context } from 'grammy'
 import type { Message } from '@grammyjs/types'
 
+// message.text contains text from regular messages sent to the bot
+// message.caption contains the caption associated with animation, audio, document, photo, video or voice messages
 export function getMainText(
-  message?: Message.TextMessage | Message.CaptionableMessage
-): string {
-  if (!message) {
-    return '';
-  }
+  message: Message.TextMessage | Message.CaptionableMessage | undefined
+): string | undefined {
+  if (message === undefined) return undefined;
   return 'text' in message ? message.text : message.caption;
 }
 
-export function getQuoteText(message?: Message.CommonMessage): string {
+export function getQuoteText(
+  message: Message.CommonMessage | undefined
+): string | undefined {
   return message?.quote?.text;
 }
 
-export function getReplyText(message?: Message.CommonMessage): string {
+export function getReplyText(
+  message: Message.CommonMessage | undefined
+): string | undefined {
   return message?.reply_to_message?.text;
 }
 
